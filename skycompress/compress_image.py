c12d88ffdfff2373e8bab3ec6152954b6660e2de
Gozgur <gozgur@novit.ai>
2023-10-03 19:00:49 +0300

    FIX: fov request corrupted image fix

--- skycompress/compress_image.py
+++ skycompress/compress_image.py
@@ -2,8 +2,19 @@ import logging
 import cv2  # type: ignore
 import numpy as np
 import numpy.typing as npt
+import datetime
+from pathlib import Path
+
 
 LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger(Path(__file__).resolve().stem)
+LOGGER.setLevel(logging.INFO)  # for debugging set logging to DEBUG
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logging.getLogger().addHandler(ch)
 
 
 def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_limit: int) -> bytearray:
@@ -20,6 +31,7 @@ def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_l
     """
 
     # Function variables
+    start_time = datetime.datetime.now()
     byte_limit = byte_limit  # 340 for Iridium, 3800 for FiPy
     jpeg_quality = 100
     color_fmt = color_fmt  # bin, gry or rgb
@@ -59,30 +71,36 @@ def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_l
     best_dimension = min_dimension
     best_size = float('inf')
 
-    while min_quality <= max_quality and min_dimension <= max_dimension:
-        # tune the mid quality option to balance what ratio you want quality & dimension
-        mid_quality = (min_quality + max_quality ) // 2
-        # tune the mid quality option to balance what ratio you want quality / dimension
-        mid_dimension = (max_dimension + max_dimension) / 2
-        
-        new_img = cv2.resize(original_img, (0, 0), fx=mid_dimension, fy=mid_dimension)
-        _, jpeg_data = cv2.imencode('.jpg', new_img, [int(cv2.IMWRITE_JPEG_QUALITY), mid_quality])
-        out_image_size = len(bytearray(jpeg_data))
-        if out_image_size <= byte_limit:
-            return jpeg_data  # Exit early if we've hit the byte limit exactly
-        elif out_image_size < byte_limit:
-            if mid_quality > best_quality:
-                best_quality = mid_quality
-                best_dimension = mid_dimension
-                best_size = out_image_size
-            min_quality = mid_quality + 10
-            min_dimension = mid_dimension + 1
-        else:
-            max_quality = mid_quality - 10
-            max_dimension = mid_dimension - 0.01
+    try:
+        while min_quality <= max_quality and min_dimension <= max_dimension:
+            # tune the mid quality option to balance what ratio you want quality & dimension
+            mid_quality = (min_quality + max_quality ) // 2
+            # tune the mid quality option to balance what ratio you want quality / dimension
+            mid_dimension = (min_dimension + max_dimension) / 2
+            
+            new_img = cv2.resize(original_img, (0, 0), fx=mid_dimension, fy=mid_dimension)
+            _, jpeg_data = cv2.imencode('.jpg', new_img, [int(cv2.IMWRITE_JPEG_QUALITY), mid_quality])
+            out_image_size = len(bytearray(jpeg_data))
+            if out_image_size == byte_limit:
+                return jpeg_data  # Exit early if we've hit the byte limit exactly
+            elif out_image_size < byte_limit:
+                if mid_quality > best_quality:
+                    best_quality = mid_quality
+                    best_dimension = mid_dimension
+                    best_size = out_image_size
+                min_quality = mid_quality + 1
+                min_dimension = mid_dimension + 0.01
+            else:
+                max_quality = mid_quality - 1
+                max_dimension = mid_dimension - 0.01
 
-    # Recreate the image with the best parameters found
-    best_img = cv2.resize(original_img, (0, 0), fx=best_dimension, fy=best_dimension)
-    _, best_jpeg_data = cv2.imencode('.jpg', best_img, [int(cv2.IMWRITE_JPEG_QUALITY), best_quality])
+        # Recreate the image with the best parameters found
+        best_img = cv2.resize(original_img, (0, 0), fx=best_dimension, fy=best_dimension)
+        _, best_jpeg_data = cv2.imencode('.jpg', best_img, [int(cv2.IMWRITE_JPEG_QUALITY), best_quality])
+        end_time = datetime.datetime.now()
+        LOGGER.info(f'image compression end time {end_time - start_time}')
+        return best_jpeg_data
+    except Exception as e:
+        LOGGER.warning(f'Failed to compress: \n {e}')
 
-    return best_jpeg_data
+
