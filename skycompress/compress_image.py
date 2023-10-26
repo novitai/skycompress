2ee0434738f090963dcce77233c96a44e4c30077
Furkan <102018762+furkan-novit@users.noreply.github.com>
2023-10-26 13:55:49 +0300

    RM: color formats (#9)
    * RM: color formats
    
    * FIX: flake8
    
    * RM: non-rgb tests
    
    * FIX: flake8
    
    * FIX: running workflow twice
    
    * FIX: flake8
    
    * FIX: flake8
    
    * ADD: ignore __init__
    
    * WIP
    
    * FIX: mypy
    
    * WIP
    
    * ADD: must type hints in function signatures
    
    * FIX: mypy
    
    * FIX: isort
    
    * WIP
    
    * WIP
    
    * Update pyproject.toml

--- skycompress/compress_image.py
+++ skycompress/compress_image.py
@@ -1,10 +1,10 @@
 import logging
-import cv2  # type: ignore
-import numpy as np
-import numpy.typing as npt
-import datetime
+import time
 from pathlib import Path
 
+import cv2  # type: ignore
+import numpy as np  # type: ignore
+import numpy.typing as npt  # type: ignore
 
 LOGGER = logging.getLogger(__name__)
 LOGGER = logging.getLogger(Path(__file__).resolve().stem)
@@ -17,74 +17,41 @@ ch.setFormatter(formatter)
 logging.getLogger().addHandler(ch)
 
 
-def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_limit: int) -> bytearray:
+def compress_image(original_img: npt.NDArray[np.float64], byte_limit: int) -> bytearray:
     """
     Function to compress the image to a set number of bytes
 
     Inputs
-    original_img = Image to be compressed, as a numpy array
-    color_fmt = String to define color format ('rgb', 'gry', 'bin')
-    bbyte_limit = Int defining max number of bytes that output image should be
+    original_img = RGB image to be compressed, as a numpy array
+    byte_limit = Int defining max number of bytes that output image should be
 
     Outputs
     jpeg_data = Compressed image, encoded as a jpeg format byte array
     """
 
     # Function variables
-    start_time = datetime.datetime.now()
+    start_time = time.perf_counter()
     byte_limit = byte_limit  # 340 for Iridium, 3800 for FiPy
     jpeg_quality = 100
-    color_fmt = color_fmt  # bin, gry or rgb
 
-    # Convert image to selected color format
-    if color_fmt == 'bin':
-        # Check if the image is of type float64 (CV_64F)
-        if original_img.dtype == np.float64:
-            # Check the range of the image values
-            max_val = original_img.max()
-            if max_val <= 1.0:
-                # If values are in the range [0, 1], rescale to [0, 255]
-                original_img = (original_img * 255).astype(np.uint8)
-            else:
-                # If values are already in the range [0, 255] or higher, just convert type
-                original_img = original_img.astype(np.uint8)
-        # Now, you can safely convert from BGR to grayscale
-        original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-
-    if color_fmt == 'gry':
-        # Check if the original_img is not already grayscale
-        if len(original_img.shape) == 3 and original_img.shape[2] == 3:
-            original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-
-        _, jpeg_data = cv2.imencode('.jpg', original_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-        jpeg_data = bytearray(jpeg_data)
-        # The following line can be removed if you're not using 'out_image_size' elsewhere
-        out_image_size = len(jpeg_data)
-        
-    if color_fmt == 'rgb':
-
-        # Save the initial image chip for a size on disk reference
-        _, jpeg_data = cv2.imencode('.jpg', original_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-        jpeg_data = bytearray(jpeg_data)
-        out_image_size = len(jpeg_data)
-
-    else:
-        LOGGER.warning("Not a valid color format")
+    # Save the initial image chip for a size on disk reference
+    _, jpeg_data = cv2.imencode('.jpg', original_img, [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
+    jpeg_data = bytearray(jpeg_data)
+    out_image_size = len(jpeg_data)
 
     min_quality, max_quality = 0, 100
     min_dimension, max_dimension = 0.1, 1.0
 
     best_quality = min_quality
     best_dimension = min_dimension
-    best_size = float('inf')
 
     try:
         while min_quality <= max_quality and min_dimension <= max_dimension:
             # tune the mid quality option to balance what ratio you want quality & dimension
-            mid_quality = (min_quality + max_quality ) // 2
+            mid_quality = (min_quality + max_quality) // 2
             # tune the mid quality option to balance what ratio you want quality / dimension
             mid_dimension = (min_dimension + max_dimension) / 2
-            
+
             new_img = cv2.resize(original_img, (0, 0), fx=mid_dimension, fy=mid_dimension)
             _, jpeg_data = cv2.imencode('.jpg', new_img, [int(cv2.IMWRITE_JPEG_QUALITY), mid_quality])
             out_image_size = len(bytearray(jpeg_data))
@@ -94,7 +61,6 @@ def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_l
                 if mid_quality > best_quality:
                     best_quality = mid_quality
                     best_dimension = mid_dimension
-                    best_size = out_image_size
                 min_quality = mid_quality + 1
                 min_dimension = mid_dimension + 0.01
             else:
@@ -104,10 +70,9 @@ def compress_image(original_img: npt.NDArray[np.float64], color_fmt: str, byte_l
         # Recreate the image with the best parameters found
         best_img = cv2.resize(original_img, (0, 0), fx=best_dimension, fy=best_dimension)
         _, best_jpeg_data = cv2.imencode('.jpg', best_img, [int(cv2.IMWRITE_JPEG_QUALITY), best_quality])
-        end_time = datetime.datetime.now()
-        LOGGER.info(f'image compression end time {end_time - start_time}')
+        end_time = time.perf_counter()
+        LOGGER.info(f'image compression took {end_time - start_time} seconds')
         return best_jpeg_data
     except Exception as e:
         LOGGER.warning(f'Failed to compress: \n {e}')
-
-
+        return bytearray(b'')
